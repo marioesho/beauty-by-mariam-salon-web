c7ba6360fa107d33993fc8db370d244388a2eaa6
Mario Esho <mario.esho@outlook.com>
2021-01-31 20:44:05 -0500

    adding spark and creating footer

--- src/app/core/components/components.module.ts
+++ src/app/core/components/components.module.ts
@@ -1,23 +1,30 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 
 import { AngularMaterialModule } from '../modules/angular-material.module';
+import { SparkAngularModule } from '../modules/spark-angular.module';
 
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { IconsComponent } from './icons/icons.component';
 
 @NgModule({
   declarations: [
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    IconsComponent
   ],
   imports: [
+    CommonModule,
     RouterModule,
-    AngularMaterialModule
+    AngularMaterialModule,
+    SparkAngularModule
   ],
   exports: [
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    IconsComponent
   ]
 })
 export class ComponentsModule { }
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 
 import { AngularMaterialModule } from './modules/angular-material.module';
+import { SparkAngularModule } from './modules/spark-angular.module';
 import { ComponentsModule } from './components/components.module';
 
 @NgModule({
   imports: [
     AngularMaterialModule,
+    SparkAngularModule,
     ComponentsModule
   ],
   exports: [
