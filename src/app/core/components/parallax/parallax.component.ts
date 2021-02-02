8e6dab5d02a5ad68ff2e57063e1639c7af5226df
Mario Esho <mario.esho@outlook.com>
2021-02-01 22:04:39 -0500

    added page title to parallax

--- src/app/core/components/parallax/parallax.component.ts
+++ src/app/core/components/parallax/parallax.component.ts
@@ -6,6 +6,7 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrls: ['./parallax.component.scss']
 })
 export class ParallaxComponent implements OnInit {
+  @Input() pageTitle!: string;
   @Input() imageUrl!: string;
   @Input() height = 850;
 
