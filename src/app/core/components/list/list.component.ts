fe99571ae9f70f2dfabb6b223048b7eef6c939d5
Mario Esho <mario.esho@outlook.com>
2021-02-02 20:52:28 -0500

    add services lists

--- src/app/core/components/list/list.component.ts
+++ src/app/core/components/list/list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { KeyValue } from '@angular/common';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-list',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./list.component.scss']
 })
 export class ListComponent implements OnInit {
+  @Input() heading!: string;
+  @Input() listItems!: KeyValue<string, string>[];
 
   constructor() { }
 
--- src/app/salon-services/salon-services.component.ts
+++ src/app/salon-services/salon-services.component.ts
@@ -1,3 +1,4 @@
+import { KeyValue } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,6 +7,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./salon-services.component.scss']
 })
 export class SalonServicesComponent implements OnInit {
+  // Column 1
+  public hairServices: KeyValue<string, string>[] = [
+    { key: 'Women\'s Haircut w/Styling', value: '$58 - $111'},
+    { key: 'Women\'s Haircut w/out Styling', value: '$48 - $88'},
+    { key: 'Men\'s Haircut', value: '$38 - $48'}
+  ];
+  public colorServices: KeyValue<string, string>[] = [
+    { key: 'One-Step Color / Root Touch-Up', value: '$73 - $108'},
+    { key: 'Two-Step Color / Root Touch-Up', value: '$83 - $128'}
+  ];
+
+  // Column 2
+  public specialtyHairServices: KeyValue<string, string>[] = [
+    { key: 'Formal Hair Style', value: '$78 - $128'},
+    { key: 'Conditioning Treatments', value: '$38 - $53'}
+  ];
+  public textureServices: KeyValue<string, string>[] = [
+    { key: 'Perms / Relaxers', value: 'Starting at $103'},
+    { key: 'Thermal Straightening', value: '$153 per hour'},
+    { key: 'Perfection Smoothing Treatment', value: 'Starting at $253'}
+  ];
 
   constructor() { }
 
