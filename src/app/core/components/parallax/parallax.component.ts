990ce7cdf399cf7ea635983e9c7613fa9336f312
Mario Esho <mario.esho@outlook.com>
2021-02-08 22:14:14 -0500

    fix parallax

--- src/app/core/components/parallax/parallax.component.ts
+++ src/app/core/components/parallax/parallax.component.ts
@@ -1,17 +1,26 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { AfterViewInit, Component, Input, OnInit } from '@angular/core';
+import 'jarallax';
+
+declare var jarallax: any;
 
 @Component({
   selector: 'app-parallax',
   templateUrl: './parallax.component.html',
   styleUrls: ['./parallax.component.scss']
 })
-export class ParallaxComponent implements OnInit {
+export class ParallaxComponent implements OnInit, AfterViewInit {
   @Input() pageTitle!: string;
   @Input() imageUrl!: string;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  ngAfterViewInit(): void {
+    jarallax(document.querySelectorAll('.jarallax'), {
+      speed: 0.2
+    });
   }
 
 }
