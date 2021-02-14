6acde059cbd84bb5e101abdfecc15a0bc202f77f
Mario Esho <mario.esho@outlook.com>
2021-02-14 13:55:12 -0500

    screen too small logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'beauty-by-mariam-salon-web';
+  public isScreenSupported = true;
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: Event): void {
+    this.isScreenSupported = (event.target as Window).innerWidth < 250 ? false : true;
+  }
 }
