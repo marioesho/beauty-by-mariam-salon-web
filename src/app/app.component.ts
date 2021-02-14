ae395e988abaaff5054e08dff03ff6578a1ec588
Mario Esho <mario.esho@outlook.com>
2021-02-14 14:04:24 -0500

    screen not supported message

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,23 @@
-import { Component, HostListener } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   public isScreenSupported = true;
+
+  ngOnInit(): void {
+    this.screenSupportedCheck();
+  }
 
   @HostListener('window:resize', ['$event'])
   onResize(event: Event): void {
-    this.isScreenSupported = (event.target as Window).innerWidth < 250 ? false : true;
+    this.screenSupportedCheck(event.target as Window);
+  }
+
+  private screenSupportedCheck(currentWindow: Window = window): void {
+    this.isScreenSupported = currentWindow.innerWidth < 250 ? false : true;
   }
 }
