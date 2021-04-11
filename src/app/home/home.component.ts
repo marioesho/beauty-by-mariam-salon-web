7dd38ab98521997acbe758b99f56d2074d8dac4e
Mario Esho <mario.esho@outlook.com>
2021-04-11 13:26:59 -0400

    Mariam updates (#8)
    * cancellation terms
    
    * cancel terms
    
    * update artists
    
    * add home page pics
    
    * remove page title
    
    * clean up image urls

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,11 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  public window: Window = window;
+  public desktopImageUrl = 'assets/images/home-desktop.jpg';
+  public mobileImageUrl = 'assets/images/home-mobile.jpg';
 
   constructor() { }
 
   ngOnInit(): void {
     window.scroll(0, 0);
   }
-
 }
